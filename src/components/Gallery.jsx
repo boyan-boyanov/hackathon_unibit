1e2244120e84d3ebf34383b6600d664d9a76e670
Boyan Boyanov <88735692+boyan-boyanov@users.noreply.github.com>
2024-11-23 23:41:20 +0200

    challenges

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -3,6 +3,7 @@ import "@fortawesome/fontawesome-free/css/all.min.css";
 
 const Gallery = () => {
   const [showQuiz, setShowQuiz] = useState(false);
+  const [showChalenge, setShowChalenge] = useState(false);
   const [randomFoods, setRandomFoods] = useState([]);
   const [player, setPlayer] = useState({
     level: 1,
@@ -12,6 +13,7 @@ const Gallery = () => {
   const [currentFood, setCurrentFood] = useState(null);
   const [answer, setAnswer] = useState("");
   const [quizMessage, setQuizMessage] = useState("");
+  const [currentChalenge, setCurrentChalenge] = useState(null);
 
   const foods = [
     { name: "Хала Ака Пухала", img: "hala" },
@@ -35,6 +37,60 @@ const Gallery = () => {
     { name: "Черимоя", img: "cherimoya" },
   ];
 
+  const challenges = [
+    { name: "Подскоци", description: "Направи 20 подскока", img: "jump" },
+    {
+      name: "Баланс на ляв крак",
+      description: "Стой на ляв крак 1 минута",
+      img: "leg",
+    },
+    {
+      name: "Баланс на десен крак",
+      description: "Стой на десен крак 1 минута",
+      img: "leg",
+    },
+    {
+      name: "Точност с хартиена топка",
+      description:
+        "Направи хартиена топка и оцели пластмасова чаша 3 пъти подред",
+      img: "paper-ball",
+    },
+    {
+      name: "Лицеви опори",
+      description: "Направи 10 лицеви опори",
+      img: "push-ups",
+    },
+    {
+      name: "Книга на главата",
+      description:
+        "Сложи книга на главата и без да я докосваш с ръце, мини през стаята.",
+      img: "book-balance",
+    },
+    {
+      name: "Танцов маратон",
+      description: "Пусни си музика и танцувай 5 минути без да спираш",
+      img: "dance",
+    },
+    {
+      name: "Цел с хартиен самолет",
+      description:
+        "Направи хартиен самолети и уцели пластмасова бутилка от 5 крачки",
+      img: "paper-plane",
+    },
+    {
+      name: "Домашен боулинг",
+      description:
+        "Използвайте 3 пластмасови бутилки за кегли и мека топка. Събори всички бутилки от 5 крачки разстояние.",
+      img: "bowling",
+    },
+    { name: "Клекове", description: "Клекни 20 пъти", img: "squats" },
+    {
+      name: "Патешко ходене",
+      description: "Обиколи стаята на патешко ходене 2 пъти",
+      img: "duck-walk",
+    },
+  ];
+
   const getRandomFoods1 = () => {
     const shuffled = [...foods].sort(() => 0.5 - Math.random());
     return shuffled.slice(0, 8);
@@ -43,6 +99,11 @@ const Gallery = () => {
   const getRandomFood = () => {
     const randomNumber = Math.floor(Math.random() * foods.length);
     return foods[randomNumber];
+  };
+
+  const getRandomChalenge = () => {
+    const randomNumber = Math.floor(Math.random() * challenges.length);
+    return challenges[randomNumber];
   };
 
   const calculateLevelUp = (exp) => {
@@ -57,11 +118,31 @@ const Gallery = () => {
 
     return { level, remainingExp: exp, nextLevelExp: expForNextLevel };
   };
+  const handleChalengeSubmit = (answer) => {
+    if (answer) {
+      const newExp = player.exp + 100;
+      const levelData = calculateLevelUp(newExp);
+
+      setPlayer((prev) => ({
+        ...prev,
+        level: levelData.level,
+        exp: levelData.remainingExp,
+        questionsAnswered: prev.questionsAnswered + 1,
+      }));
+      setQuizMessage("Браво!Ти спечели 100 точки.");
+    } else {
+      setQuizMessage("Не се отказвай! Другият път ще успееш.");
+    }
+
+    // Show a new chalenge
+    setCurrentChalenge(getRandomChalenge());
+    setAnswer("");
+  };
 
   const handleAnswerSubmit = () => {
     if (player.questionsAnswered >= 10) {
       setQuizMessage(
-        "You have answered all 10 questions for today! Try again tomorrow."
+        "Ти отговори на всичките 10 въпроса за днес! Пробвай пак утре."
       );
       setShowQuiz(false);
       return;
@@ -78,9 +159,9 @@ const Gallery = () => {
         exp: levelData.remainingExp,
         questionsAnswered: prev.questionsAnswered + 1,
       }));
-      setQuizMessage("Correct! You've earned 100 EXP.");
+      setQuizMessage("Правилно!Ти спечели 100 точки.");
     } else {
-      setQuizMessage("Incorrect! Try again.");
+      setQuizMessage("Неправилно! Пробвай пак.");
     }
 
     // Show a new question
@@ -92,17 +173,22 @@ const Gallery = () => {
     setRandomFoods(getRandomFoods1());
   }, []);
 
-  const startQuiz = () => {
-    console.log("startQuiz");
+  const startChalenge = () => {
+    setCurrentChalenge(getRandomChalenge());
+    setShowChalenge(true);
+    setShowQuiz(false);
+  };
 
+  const startQuiz = () => {
     if (player.questionsAnswered >= 10) {
       setQuizMessage(
-        "You have answered all 10 questions for today! Try again tomorrow."
+        "Ти отговори на всичките 10 въпроса за днес! Пробвай пак утре."
       );
       return;
     }
 
     setCurrentFood(getRandomFood());
+    setShowChalenge(false);
     setShowQuiz(true);
   };
   console.log(currentFood);
@@ -128,31 +214,31 @@ const Gallery = () => {
 
           <ul className="tm-gallery-links">
             <li>
-              <a href="/;" className="active tm-gallery-link" data-filter="*">
+              <a className="active tm-gallery-link" data-filter="*">
                 <i className="fas fa-layer-group tm-gallery-link-icon"></i>
                 Всички
               </a>
             </li>
-            <li>
-              <a href="/;" className="tm-gallery-link" data-filter="portrait">
+            <li onClick={() => startChalenge()}>
+              <a className="tm-gallery-link" data-filter="portrait">
                 <i className="fas fa-portrait tm-gallery-link-icon"></i>
                 Спорт
               </a>
             </li>
             <li>
-              <a href="/;" className="tm-gallery-link" data-filter="nature">
+              <a className="tm-gallery-link" data-filter="nature">
                 <i className="fas fa-leaf tm-gallery-link-icon"></i>
                 Плодове
               </a>
             </li>
             <li>
-              <a href="/;" className="tm-gallery-link" data-filter="animal">
+              <a className="tm-gallery-link" data-filter="animal">
                 <i className="fas fa-paw tm-gallery-link-icon"></i>
                 Зеленчуци
               </a>
             </li>
             <li onClick={() => startQuiz()}>
-              <a href="#" className="tm-gallery-link" data-filter="building">
+              <a className="tm-gallery-link" data-filter="building">
                 <i className="far fa-building tm-gallery-link-icon"></i>
                 Викторина
               </a>
@@ -186,19 +272,49 @@ const Gallery = () => {
           <button className="close-modal" onClick={() => setShowQuiz(false)}>
             X
           </button>
-          <h3>What is the name of this food?</h3>
-          <img
-            src={`/assets/img/foods/${currentFood.img}.jpg`}
-            alt={currentFood.name}
-          />
-          <input
-            type="text"
-            value={answer}
-            onChange={(e) => setAnswer(e.target.value)}
-            placeholder="Enter your answer"
-          />
-          <button onClick={handleAnswerSubmit}>Submit</button>
-          <p>{quizMessage}</p>
+          <div className="quiz-modal-content">
+            <h3 className="quiz-modal-question">
+              Какво е името на тази храна?
+            </h3>
+            <img
+              src={`/assets/img/foods/${currentFood.img}.jpg`}
+              alt={currentFood.name}
+            />
+            <input
+              type="text"
+              value={answer}
+              onChange={(e) => setAnswer(e.target.value)}
+              placeholder="Въведи своя отговор"
+            />
+            <button onClick={handleAnswerSubmit}>Изпрати</button>
+            <p>{quizMessage}</p>
+          </div>
+        </div>
+      )}
+
+      {/* Chalenges Modal */}
+      {showChalenge && (
+        <div className="quiz-modal">
+          <button
+            className="close-modal"
+            onClick={() => setShowChalenge(false)}
+          >
+            X
+          </button>
+          <div className="quiz-modal-content">
+            <h1 className="quiz-modal-question">{currentChalenge.name}</h1>
+            <h3>{currentChalenge.description}</h3>
+            <img
+              src={`/assets/img/chalenges/${currentChalenge.img}.png`}
+              alt={currentFood.name}
+            />
+            <h5>Успя ли да завършиш предизвикателството?</h5>
+            <div className="quiz-modal-buttons">
+              <button onClick={() => handleChalengeSubmit(true)}>Да</button>
+              <button onClick={() => handleChalengeSubmit(false)}>Не</button>
+            </div>
+            <p>{quizMessage}</p>
+          </div>
         </div>
       )}
     </>
